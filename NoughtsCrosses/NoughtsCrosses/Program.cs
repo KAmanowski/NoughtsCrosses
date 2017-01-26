dd9bf5a4e0e8f56c9e51a15a0de38c30d6538dc8
Jordan <Jordan@DESKTOP-8M5E3JU>
2017-01-26 22:39:40 +0000

    start

--- NoughtsCrosses/NoughtsCrosses/Program.cs
+++ NoughtsCrosses/NoughtsCrosses/Program.cs
@@ -10,7 +10,28 @@ namespace NoughtsCrosses
     {
         static void Main(string[] args)
         {
-          
+            String Name;
+            
+
+            Console.WriteLine("Welocome to Noughts&Crosses");
+            Console.WriteLine("Lets Start shall we?");
+            Console.WriteLine("Type 'Y'to begin or type 'N' to exit");
+            Console.ReadLine();
+           if(Console.ReadLine() == Y)
+            {
+                Console.Clear();
+                Console.WriteLine("Great! Lets get started!");
+            }
+           else if (Console.ReadLine() == N)
+            {
+                Console.WriteLine("Awe thats a shame! Maybe next time?!");
+                Console.ReadLine();
+                Environment.Exit;
+            }
+
+
+
+        
         }
     }
 }
