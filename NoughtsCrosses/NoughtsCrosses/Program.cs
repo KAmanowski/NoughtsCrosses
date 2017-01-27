a1936334ea8016b20cfc29a52c39f05017081ae9
Kacper Amanowski <kacper3@yahoo.co.uk>
2017-01-27 21:27:29 +0000

    Allows players to enter their names, validates this input and displays them.

--- NoughtsCrosses/NoughtsCrosses/Program.cs
+++ NoughtsCrosses/NoughtsCrosses/Program.cs
@@ -8,18 +8,44 @@ namespace NoughtsCrosses
 {
     class Program
     {
+        private static string _p1Name, _p2Name; // player names
+        private static string[,] _boardpos = new string[3, 3]; // positions on the board
+
+
         static void Main(string[] args)
         {
-            string[,] _boardpos = new string[3, 3]; //positions on the board
-            string input;
+            string input, _nameError = "Please enter a valid name."; // input and error message
 
-            ClearBoard(ref _boardpos);
-            Draw(_boardpos);
+            ClearBoard();
+
+            Console.WriteLine("Welcome to the Noughts & Crosses game.");
+
+            for (int x = 1; x < 3; x++)
+            {
+                Console.Write("Player {0} name:", x);
+                input = Console.ReadLine();
+
+                if (ValidateStringInput(input))
+                {
+                    if (x == 1)
+                        _p1Name = input;
+                    if (x == 2)
+                        _p2Name = input;
+                }
+                else { x--; }
+            }
+            Console.Clear();
+            DisplayPlayerNames();
             Console.ReadLine();
 
         }
 
-        static void Draw(string[,] _boardpos)
+        static void DisplayPlayerNames() // displays player names
+        {
+            Console.WriteLine("Player 1: {0}        Player 2: {1}", _p1Name, _p2Name);
+        }
+
+        static void Draw() // draws the board
         {
             Console.WriteLine(" {0} | {1} | {2}", _boardpos[0, 0], _boardpos[1, 0], _boardpos[2, 0]);
             Console.WriteLine("-----------");
@@ -29,7 +55,7 @@ namespace NoughtsCrosses
 
         }
 
-        static void ClearBoard(ref string[,] _boardpos)
+        static void ClearBoard() // clears the board
         {
             for (int x = 0; x < 3; x++)
             {
@@ -38,6 +64,19 @@ namespace NoughtsCrosses
                     _boardpos[x, y] = " ";
                 }
 
+            }
+        }
+
+        static bool ValidateStringInput(string input) // validates whether or not the entered string is valid
+        {
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
             }
         }
     }
